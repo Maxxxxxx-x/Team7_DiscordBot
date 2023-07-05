349ed3a4db0c69aea7449e932160e94705db0d22
henryleecode23 <99625462+henryleecode23@users.noreply.github.com>
2023-07-05 21:54:00 +0800

    feat: fish cmd
    not completed

---
+++ src/Commands/fish.js
@@ -0,0 +1,29 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('fish')
+        .setDescription('start fishing!'),
+    async execute(Bot, interaction) {
+        const message = await interaction.reply({ content: 'fishing...', fetchReply: true, ephemeral: true });
+        await interaction.wait(Math.floor(Math.random() * 10000) + 1000);
+        const jsonData = fs.readFileSync("./data/pond.json");
+        let pond = JSON.parse(jsonData);
+        await interaction.editReply({
+            content: "",
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle(":fishing_pole_and_fish: Fishing!")
+                    .addFields(
+                        {
+                            name: ":fish: Fish",
+                            value: `You caught a ${pond.fishes[Math.floor(Math.random() * pond.fishes.length)]}!`,
+                            inline: false
+                        }
+                    )
+                    .setColor("Green")
+            ]
+        });
+    }
+};
