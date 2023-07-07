8c4179e541f924bb4dfb962166caca27d061e722
A-Dreemurr <134613472+A-Dreemurr@users.noreply.github.com>
2023-07-07 11:55:49 +0800

    bday(未完成)

--- src/Commands/bday.js
+++ src/Commands/bday.js
@@ -1,35 +1,43 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder, Client } = require('discord.js');
+const fs = require('node:fs');
+
+//[建立/回覆 button] -> [建立 collector] -> [輸贏啦] -> [讀檔] -> [解析] -> [做事]  -> [回應] -> [存檔]
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('bday!')
+        .setName('bday')
         .setDescription('為頻道裡的用戶慶祝生日'),
-    async execute(Bot, Interaction) {
-        const Message = await Interaction.reply({ content: '壽星在哪裡?!', fetchReply: true });
-        await Interaction.editReply({
-            content: "",
-            embeds: [
-                new EmbedBuilder()
-                    .setTitle("")
-                    .addFields(
-                        {
-                            name: ":stopwatch: Uptime",
-                            value: `${Math.round(Interaction.client.uptime / 60000)} minutes`,
-                            inline: false
-                        },
-                        {
-                            name: ":sparkling_heart: Websocket heartbeat",
-                            value: `${Interaction.client.ws.ping}ms`,
-                            inline: false
-                        },
-                        {
-                            name: ":round_pushpin: Roundtrip Latency",
-                            value: `${Message.createdTimestamp - Interaction.createdTimestamp}ms`,
-                            inline: false
-                        }
-                    )
-                    .setColor("Random")
-            ]
+    async execute(client, interaction) {
+
+        //建立 embed 和 button
+        const buttonEmbed = new EmbedBuilder()
+            .setColor('#5865F2')
+            .setTitle(`今日壽星是...`);
+
+        const scissorButton = new ButtonBuilder()
+            .setCustomId('')
+            .setLabel('生日快樂🥳')
+            .setStyle(ButtonStyle.Primary);
+
+        //將 button 都放入 row 中並回覆 embed 和 row
+        const buttonRow = new ActionRowBuilder()
+            .addComponents(
+                celebrateButton
+            );
+        
+        //回覆
+        interaction.reply({ embeds: [buttonEmbed], components: [buttonRow] });
+
+        //建立 collector
+        const collector = interaction.channel.createMessageComponentCollector({ time: 180000 });
+
+        //等待 collector 蒐集到玩家按的按鈕
+        collector.on('collect', async collected => {
+            await interaction.reply(``);
         });
     }
+    //如果符合:發送通知(Embed+button)
+
+    //A按下button:A祝壽星生日快樂!
 };
+setColor("Random")
--- src/Modules/database.js
+++ src/Modules/database.js
@@ -1,6 +1,44 @@
-//用戶輸入日期=>進入資料庫
 const sqlite3 = require("sqlite3").verbose();
+const path = require("node:path");
 
+const db = new sqlite3.Database(path.join(__dirname, "sample.db"), sqlite3.OPEN_READWRITE, (error) => {
+    if (error){
+        return console.error(error);
+    }
+    console.log("Connected to database!");
+})
+
+let sql1 = `
+CREATE TABLE IF NOT EXISTS Users(   
+    MemberID TEXT PRIMARY_KEY,
+    month INT
+    date INT
+);
+`
+//用戶輸入日期=>進入資料庫
+Bot.on("guildMemberAdd", async (MemberID) => {
+    let sql3 =`
+    INSERT INTO Users (MemberID,birthmonth,birthdate) //插入新資料 (行1,行2) 數值(值1,值2)
+    VALUES (UsersID,Usersmonth,Usersdate);
+    `
+});
+//用戶離開
+Bot.on("guildMemberRemove", async (MemberID) => {
+    let sql7 = `
+    DELETE FROM Users
+    WHERE ID = "MemberID";
+    `
+    db.exec(sql7);
+})
 //每天日期增加=>搜尋資料庫
-//如果符合:發送通知(Embed+button)
-//A按下button:A祝壽星生日快樂!
+const hour = dateObject.getHours()
+while(hour==0){
+    const date = dateObject.getDate();
+    const month = dateObject.getMonth();  
+}
+
+let sql5 = `
+SELECT * FROM Users
+WHERE birthdate = date && birthmonth = month;
+`
+
