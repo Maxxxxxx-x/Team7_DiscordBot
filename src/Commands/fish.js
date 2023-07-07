c5226c9e242da82c890153d62acf8da9e6f9b304
taylor0306 <131685432+taylor0306@users.noreply.github.com>
2023-07-07 11:14:25 +0800

    fish
    fishing successful

--- src/Commands/fish.js
+++ src/Commands/fish.js
@@ -7,23 +7,24 @@ module.exports = {
         .setDescription('start fishing!'),
     async execute(Bot, interaction) {
         const message = await interaction.reply({ content: 'fishing...', fetchReply: true, ephemeral: true });
-        await interaction.wait(Math.floor(Math.random() * 10000) + 1000);
-        const jsonData = fs.readFileSync("./data/pond.json");
-        let pond = JSON.parse(jsonData);
-        await interaction.editReply({
-            content: "",
-            embeds: [
-                new EmbedBuilder()
-                    .setTitle(":fishing_pole_and_fish: Fishing!")
-                    .addFields(
-                        {
-                            name: ":fish: Fish",
-                            value: `You caught a ${pond.fishes[Math.floor(Math.random() * pond.fishes.length)]}!`,
-                            inline: false
-                        }
-                    )
-                    .setColor("Green")
-            ]
-        });
+        setInterval(()=>{
+            const jsonData = fs.readFileSync("./data/pond.json");
+            let pond = JSON.parse(jsonData);
+            interaction.editReply({
+                content: "",
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle(":fishing_pole_and_fish: Fishing!")
+                        .addFields(
+                            {
+                                name: ":fish: Fish",
+                                value: `You caught a ${pond.fishes[Math.floor(Math.random() * pond.fishes.length)]}!`,
+                                inline: false
+                            }
+                        )
+                        .setColor("Green")
+                ]
+            });
+        }, (Math.floor(Math.random() * 10000) + 1000));
     }
 };
