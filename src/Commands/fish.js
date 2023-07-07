863fdc14b7f5a79ec86a7b62ffe9ccfed87fadff
taylor0306 <131685432+taylor0306@users.noreply.github.com>
2023-07-07 11:50:01 +0800

    fish to candy
    flavor

--- src/Commands/fish.js
+++ src/Commands/fish.js
@@ -3,28 +3,25 @@ const fs = require("fs");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('fish')
-        .setDescription('start fishing!'),
+        .setName('candy')
+        .setDescription('start drawing a candy!'),
     async execute(Bot, interaction) {
-        const message = await interaction.reply({ content: 'fishing...', fetchReply: true, ephemeral: true });
-        setInterval(()=>{
+        const message = await interaction.reply({ content: 'drawing...', fetchReply: true, ephemeral: true });
+        let Delay = (Math.floor(Math.random() * 10000) + 1000);
+        console.log(Delay);
+        setTimeout(()=>{
             const jsonData = fs.readFileSync("./data/pond.json");
             let pond = JSON.parse(jsonData);
             interaction.editReply({
                 content: "",
                 embeds: [
                     new EmbedBuilder()
-                        .setTitle(":fishing_pole_and_fish: Fishing!")
-                        .addFields(
-                            {
-                                name: ":fish: Fish",
-                                value: `You caught a ${pond.fishes[Math.floor(Math.random() * pond.fishes.length)]}!`,
-                                inline: false
-                            }
-                        )
+                        .setTitle(":candy: 七七乳加")
+                        .setDescription( `獲得一個 ${pond.candy[Math.floor(Math.random() * pond.candy.length)]}口味的七七乳加!`)
                         .setColor("Green")
                 ]
             });
-        }, (Math.floor(Math.random() * 10000) + 1000));
+            return;
+        }, Delay);
     }
 };
